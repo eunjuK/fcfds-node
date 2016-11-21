0f73450c46dee2519446cc9247550c40416458b1
eunjuK <miro0oa5@gmail.com>
2016-11-21 16:30:10 +0900

    add hom route: add home route in app.js print request.url at console

--- firstnodeserver/app.js
+++ firstnodeserver/app.js
@@ -3,10 +3,16 @@ var http = require("http");
 // http Status(200): OK, send response "hello world"
 // 웹 서버에는 요청과 응답만 있음
 // 요청과 응답을 참조값으로 넣어줌
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello world! \n");
-	response.end();
+var app = http.createServer(function(request, response) {
+	// response.writeHead(200, {"Content-Type": "text/plain"});
+	// response.write("Hello world! \n");
+	// response.end();
+	console.log(request.url);
+
+	if(request.url === "/") {
+		response.write("This is HOME!");
+		response.end();
+	}
 }).listen(process.env.PORT || 3030);
 
 // 서버가 동작했는지 확인하기위함.
