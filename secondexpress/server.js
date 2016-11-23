25da8df09cf56b6dc761fdc5d637340d6a369e6a
eunjuK <miro0oa5@gmail.com>
2016-11-23 17:04:15 +0900

    add /routes/chat.js

--- secondexpress/server.js
+++ secondexpress/server.js
@@ -4,6 +4,7 @@ var app = express();
 var indexRouter = require("./routes/index")(app);
 var aboutRouter = require("./routes/about")(app);
 var userRouter = require("./routes/users")(app);
+var chatRouter = require("./routes/chat")(app);
 
 
 app.set("port", process.env.PORT ||3030);
