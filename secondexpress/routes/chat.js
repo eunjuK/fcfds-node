6bccb5b48db97da78a9193b800b079917d71ab5b
eunjuK <miro0oa5@gmail.com>
2016-11-24 15:43:07 +0900

    socket communication.

--- secondexpress/routes/chat.js
+++ secondexpress/routes/chat.js
@@ -1,4 +1,32 @@
 module.exports = function(app) {
+	// 16.11.24
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+
+	app.get('/chat', function(req, res){
+		res.render("chat.ejs");
+	});
+
+	io.on("connection", function(socket){
+		console.log("New user connected!");
+
+		socket.on("chat message", function(msg){
+			console.log("message: " + msg);
+			io.emit("chat message", msg);
+		});
+
+		socket.on("disconnect", function(){
+			console.log("User disconnected");
+		});
+	});
+
+	http.listen(process.env.PORT || 8080, function(){
+		console.log("Chat Server is running...");
+	});
+
+
+	// 16.11.23
+	/*
 	var http = require('http').Server(app);
 	var io = require('socket.io')(http);
 
@@ -14,9 +42,8 @@ module.exports = function(app) {
 	});
 
 
-	/*
-	app.get('/chat', function(req, res){
-		res.send("chat server start");
-	});
+	// app.get('/chat', function(req, res){
+	//	res.send("chat server start");
+	// });
 	*/
 }
